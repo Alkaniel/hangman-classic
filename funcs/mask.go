a8877a326717adaf291c110606d02bd4299178e7
Enzo Giardinelli <enzogiardinelli@icloud.com>
2024-10-30 10:04:22 +0100

    fix

--- funcs/mask.go
+++ funcs/mask.go
@@ -7,20 +7,23 @@ import (
 )
 
 /** Fonction qui masque mon mot en laissant un n nombre de lettre. N = (longeur du mot)/2 - 1 */
-	func MaskWord(toFind string) string {
-		var masked string // Celle qui vas être return
-		var notmask []string //Liste de lettre à retirer
-		n := len(toFind)/2 - 1 // Le nombre de lettre qui doit être reveal dans le mot
-		for i:= 0; i < n; i++{   //La boucle s'arrête au nombre de 
-			j := rand.Intn(len(toFind)-1) //Choisi un nombre aléatoire sur les mots
-			notmask = append(notmask, string(toFind[j])) 
-		}
-		for _, letter := range toFind { //Liste pour masquer le mot
-			if helpers.ContainsForHangman(notmask, string(letter)) { //Check si la rune du mot est la lune de la lettre.
-				masked += strings.ToUpper(string(letter))
-			} else {
-				masked += "_"
-			}
-		}
-		return masked
+func MaskWord(toFind string) string {
+	var masked string      // Celle qui vas être return
+	var notmask []string   //Liste de lettre à retirer
+	n := len(toFind)/2 - 1 // Le nombre de lettre qui doit être reveal dans le mot
+	if n == 0 {
+		n = 1
+	} //Si le mot est trop court, on met 1 lettre
+	for i := 0; i < n; i++ { //La boucle s'arrête au nombre de
+		j := rand.Intn(len(toFind) - 1) //Choisi un nombre aléatoire sur les mots
+		notmask = append(notmask, string(toFind[j]))
 	}
+	for _, letter := range toFind { //Liste pour masquer le mot
+		if helpers.ContainsForHangman(notmask, string(letter)) { //Check si la rune du mot est la lune de la lettre.
+			masked += strings.ToUpper(string(letter))
+		} else {
+			masked += "_"
+		}
+	}
+	return masked
+}
